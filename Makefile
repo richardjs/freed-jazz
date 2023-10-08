963cb9a6c83ca8ed6d2f543e3a95a4f9362556a4
Richard Schneider <richard@schneiderbox.net>
2023-10-08 18:38:10 -0500

    Create pipeline for rendering scores

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
+render:
+	scripts/render_scores.py
+
+clean:
+	rm -f content/songs/*/*.pdf
+	rm -f content/songs/*/*.mscz
+	rm -f content/songs/*/*.svg
+	rm -f content/songs/*/*.png
+
 fmt:
 	find layouts/ -name *.html | xargs prettier -w
 	find content/ -name *.md | xargs prettier -w
---
+++ scripts/render_scores.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path
+
+SCORES_DIR = Path("scores")
+SONGS_DIR = Path("content") / "songs"
+
+for song in os.listdir(SCORES_DIR):
+    score_dir = SCORES_DIR / song
+    mscx_path = score_dir / f"{song}.mscx"
+
+    mscx_mtime = mscx_path.stat().st_mtime
+
+    song_dir = SONGS_DIR / song
+
+    png_path = song_dir / f"{song}.png"
+    output_paths = [
+        song_dir / f"{song}.pdf",
+        song_dir / f"{song}.mscz",
+        song_dir / f"{song}.svg",
+        png_path,
+    ]
+
+    for output_path in output_paths:
+        if output_path.exists() and output_path.stat().st_mtime >= mscx_mtime:
+            print(f"{output_path} is up to date.")
+            continue
+
+        print(f"Rendering {output_path}...")
+        os.system(f"mscore -o {output_path} {mscx_path}")
+
+        if output_path == png_path:
+            # mscore generates separate PNGs for each page; we only have
+            # single-page sheets right now, so remove the page suffix
+            os.system(f"mv { song_dir / '*-1.png' } { png_path }")
+
+            # Trim PNG
+            os.system(f"magick { png_path } -trim { png_path }")
