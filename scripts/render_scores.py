917cca7492239917b767fe05eb7fc8873ea08114
Richard Schneider <richard@schneiderbox.net>
2024-05-26 18:47:22 -0500

    Begin work creating transposed score renders

--- scripts/render_scores.py
+++ scripts/render_scores.py
@@ -2,9 +2,56 @@
 
 import os
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
 SCORES_DIR = Path("scores")
 SONGS_DIR = Path("content") / "songs"
+
+
+KEYS = {
+    -6: "Gb",
+    -5: "Db",
+    -4: "Ab",
+    -3: "Eb",
+    -2: "Bb",
+    -1: "F",
+    0: "C",
+    1: "G",
+    2: "D",
+    3: "A",
+    4: "E",
+    5: "B",
+    6: "F#",
+    7: "C#",
+}
+
+
+def transpose(mscx_path, to_key_num):
+    tree = ET.parse(mscx_path)
+    root = tree.getroot()
+
+    key_elem = root.find("./Score/Staff/Measure/voice/KeySig/concertKey")
+    key_num = int(key_elem.text)
+
+    key_elem.text = str(to_key_num)
+
+    steps = (key_num - to_key_num) * 7 % 12
+    if steps > 6:
+        steps -= 11
+
+    for note_elem in root.findall("./Score/Staff/Measure/voice/Chord/Note"):
+        pitch_elem = note_elem.find("pitch")
+        pitch_elem.text = str(int(pitch_elem.text) - steps)
+
+        # TODO We probably should be properly dealing with this,
+        # instead of just removing it.
+        # https://musescore.github.io/MuseScore_PluginAPI_Docs/plugins/html/tpc.html
+        note_elem.remove(note_elem.find("tpc"))
+
+    # TODO Change chord symbols
+
+    tree.write("/tmp/output.mscx")
+
 
 for song in os.listdir(SCORES_DIR):
     score_dir = SCORES_DIR / song
